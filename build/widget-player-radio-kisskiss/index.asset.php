515240100973b7d0cebf104cffddc056cc1c1fda
Simone D'Anna <31928695+simone7121@users.noreply.github.com>
2026-05-27 11:55:52 +0200

    rimossa build

--- build/widget-player-radio-kisskiss/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'a2af0ddf3f17c195d0b4');
--- build/widget-player-radio-kisskiss/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '0930e36103162fb5b589');
